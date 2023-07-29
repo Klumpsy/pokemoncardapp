827dcc86dd56eea29b0b456a4b37dcb98c501b3f
bart <bart_klumperman@Live.nl>
2023-07-29 20:48:32 +0200

    added api call

--- src/Pages/CardSets/CardSets.jsx
+++ src/Pages/CardSets/CardSets.jsx
@@ -1,10 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// import CardSet from './CardSet'; // Assuming you have a CardSet component
+
 const CardSets = () => {
- 
-    return (
-        <div>
-            <h1>CardSets</h1>
-        </div>
-    );
-  };
-  
-  export default CardSets;
+    const [cardSets, setCardSets] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios('https://api.pokemontcg.io/v2/sets', {
+                headers: {
+                    'Authorization': `Bearer ${process.env.REACT_APP_TCG_API_KEY}`
+                }
+            });
+            setCardSets(result.data.data);
+        }
+
+        fetchData();
+    }, []);
+
+        return (
+            <div>
+                <h1>CardSets</h1>
+                { cardSets.map(cardSet => (
+                    <div key={cardSet.id}>
+                        <h2>{cardSet.name}</h2>
+                        <img src={cardSet.images.logo} alt={cardSet.name}/>
+                    </div>
+                ))}
+            </div>
+        );
+};
+
+export default CardSets;
